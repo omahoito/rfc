0cd6940df4f8cb6a86b503f5897c4222a3877c72
Omar El-Begawy <omar.elbegawy@gmail.com>
2017-04-10 14:25:39 +0300

    added json validation for fhir resources

---
+++ jsonvalidator.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+# validation of FHIR resource with JSON Spec: https://json-spec.readthedocs.io/
+
+unset jmap
+
+declare -A jmap
+
+JSD_PATH="json_schemas/"
+APPOINTMENT_PATH="Appointment/"
+
+jmap["ODA-activitydefinition.example.json"]
+jmap["ODA-appointment.example.json"]="$JSD_PATH""fhir.schema.json"
+jmap["ODA-careplan.example.json"]="$JSD_PATH""fhir.schema.json"
+jmap["ODA-careteam.example.json"]="$JSD_PATH""fhir.schema.json"
+jmap["ODA-communication.example.json"]="$JSD_PATH""fhir.schema.json"
+jmap["ODA-consent.example.json"]="$JSD_PATH""fhir.schema.json"
+jmap["ODA-device.example.json"]="$JSD_PATH""fhir.schema.json"
+jmap["ODA-finnishpatient-patient.example.json"]="$JSD_PATH""fhir.schema.json"
+jmap["ODA-medicationstatement.example.json"]="$JSD_PATH""fhir.schema.json"
+jmap["ODA-person.example.json"]="$JSD_PATH""fhir.schema.json"
+jmap["ODA-practitioner.example.json"]="$JSD_PATH""fhir.schema.json"
+jmap["ODA-questionnaire.example.json"]="$JSD_PATH""fhir.schema.json"
+jmap["ODA-questionnaireresponse.example.json"]="$JSD_PATH""fhir.schema.json"
+jmap["ODA-temperature-observation.example.json"]="$JSD_PATH""fhir.schema.json"
+jmap["ODA-temperature-quantity.example.json"]="$JSD_PATH""fhir.schema.json"
+jmap["questionnaireresponse-example.json"]="$JSD_PATH""fhir.schema.json"
+
+jmap["$APPOINTMENT_PATH""ODAAppointment-example.json"]="$JSD_PATH""fhir.schema.json"
+jmap["$APPOINTMENT_PATH""ODAHealthcareService-example.json"]="$JSD_PATH""fhir.schema.json"
+jmap["$APPOINTMENT_PATH""ODAOrganization-example.json"]="$JSD_PATH""fhir.schema.json"
+jmap["$APPOINTMENT_PATH""ODAPatient-example.json"]="$JSD_PATH""fhir.schema.json"
+jmap["$APPOINTMENT_PATH""ODAPractitioner-example.json"]="$JSD_PATH""fhir.schema.json"
+
+RETVAL=0
+for key in ${!jmap[@]}
+do
+#  echo $key
+  json validate --schema-file="${jmap[$key]}" --document-file="$key"
+  OP=$?
+  echo $OP
+  if [ $OP -ne 0 ]
+  then
+    echo "${jmap[$key]}"" did not pass validation"
+    RETVAL=1
+  else
+    echo "${jmap[$key]}"" passed validation"
+  fi
+done
